ab0cbc89ae298745f52384e88c880fc07a85170b
Brian Kelley <brian.kelley@jamf.com>
2018-05-29 13:12:31 -0500

    Add command line options to set image and namespace

--- samcli/commands/local/start_api/cli.py
+++ samcli/commands/local/start_api/cli.py
@@ -39,12 +39,18 @@ and point SAM to the directory or file containing build artifacts.
               default="public",
               help="Any static assets (e.g. CSS/Javascript/HTML) files located in this directory "
                    "will be presented at /")
+@click.option("--image", "-i",
+              default="",
+              help="Image to use")
+@click.option("--namespace", "-n",
+              default="default",
+              help="Kubernetes namespace to create pod within")
 @invoke_common_options
 @cli_framework_options
 @pass_context
 def cli(ctx,
         # start-api Specific Options
-        host, port, static_dir,
+        host, port, static_dir, image, namespace,
 
         # Common Options for Lambda Invoke
         template, env_vars, debug_port, debug_args, docker_volume_basedir,
@@ -53,11 +59,11 @@ def cli(ctx,
     # All logic must be implemented in the ``do_cli`` method. This helps with easy unit testing
 
     do_cli(ctx, host, port, static_dir, template, env_vars, debug_port, debug_args, docker_volume_basedir,
-           docker_network, log_file, skip_pull_image, profile)  # pragma: no cover
+           docker_network, log_file, skip_pull_image, profile, image, namespace)  # pragma: no cover
 
 
 def do_cli(ctx, host, port, static_dir, template, env_vars, debug_port, debug_args,  # pylint: disable=R0914
-           docker_volume_basedir, docker_network, log_file, skip_pull_image, profile):
+           docker_volume_basedir, docker_network, log_file, skip_pull_image, profile, image, namespace):
     """
     Implementation of the ``cli`` method, just separated out for unit testing purposes
     """
@@ -77,7 +83,9 @@ def do_cli(ctx, host, port, static_dir, template, env_vars, debug_port, debug_ar
                            docker_network=docker_network,
                            log_file=log_file,
                            skip_pull_image=skip_pull_image,
-                           aws_profile=profile) as invoke_context:
+                           aws_profile=profile,
+                           image=image,
+                           namespace=namespace) as invoke_context:
 
             service = LocalApiService(lambda_invoke_context=invoke_context,
                                       port=port,
